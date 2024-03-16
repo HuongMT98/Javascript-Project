bb851ddd01f75e2387ce62de44072f17cfb94074
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2024-03-16 11:45:02 +0700

    App navbar show

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //                                      COUNT
-
 const numCount = document.querySelector('#numcount')
 const countTangNum = document.querySelector('#countTangNum')
 const countGiamNum = document.querySelector('#countGiamNum')
@@ -19,3 +18,29 @@ resetNumber.onclick = function () {
     number = 0
     numCount.textContent = number
 }
+
+
+//                                      ĐÁNH ĐỀ
+
+
+const soDe = document.querySelector('#soDe')
+const btnDanhDe = document.querySelector('#btnDanhDe')
+const btnResetDanhDe = document.querySelector('#btnResetDanhDe')
+
+
+btnDanhDe.onclick = function () {
+    let randomNum = Math.floor(Math.random() * 100) + 1
+    soDe.innerHTML = `Đánh con: ${randomNum} `
+}
+btnResetDanhDe.onclick = function () {
+    soDe.innerHTML = 'Đánh Đề Con Gì Đây'
+}
+
+//                                     NAVBAR
+let daddy = document.querySelectorAll('.daddy')
+daddy.forEach(e => {
+    e.addEventListener('click', () => {
+        let child = e.querySelector('.child')
+        child.classList.toggle('active')
+    })
+})

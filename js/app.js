a43025b35b85f06e9b18e54e25e33669e28f298e
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2024-03-16 18:46:21 +0700

    Xúc Xắc

--- js/app.js
+++ js/app.js
@@ -30,17 +30,64 @@ const btnResetDanhDe = document.querySelector('#btnResetDanhDe')
 
 btnDanhDe.onclick = function () {
     let randomNum = Math.floor(Math.random() * 100) + 1
-    soDe.innerHTML = `Đánh con: ${randomNum} `
+    soDe.innerHTML = `Đánh con: ${randomNum} này đi`
 }
 btnResetDanhDe.onclick = function () {
     soDe.innerHTML = 'Đánh Đề Con Gì Đây'
 }
 
+
 //                                     NAVBAR
-let daddy = document.querySelectorAll('.daddy')
-daddy.forEach(e => {
+let a = document.querySelectorAll('.daddy')
+a.forEach(e => {
     e.addEventListener('click', () => {
-        let child = e.querySelector('.child')
-        child.classList.toggle('active')
-    })
+        let b = e.querySelector('.child')
+        b.classList.toggle('active')
+    });
 })
+
+
+
+
+//                                     CHUYỂN NHIỆT ĐỘ
+const textBox = document.querySelector('#textBox')
+const sangDoF = document.querySelector('#sangDoF')
+const sangDoC = document.querySelector('#sangDoC')
+const ketQuaChuyen = document.querySelector('#ketQuaChuyen')
+let nhietDo
+
+
+
+function chuyenSo() {
+    if (sangDoF.checked) {
+        nhietDo = Number(textBox.value)
+        nhietDo = nhietDo * 9 / 5 + 32
+        ketQuaChuyen.innerHTML = nhietDo.toFixed(1) + "F"
+    }
+    else if (sangDoC.checked) {
+        nhietDo = Number(textBox.value)
+        nhietDo = (nhietDo - 32) * (5 / 9)
+        ketQuaChuyen.innerHTML = nhietDo.toFixed(1) + "C"
+    } else {
+        ketQuaChuyen.innerHTML = "Vui lòng chọn loại độ nhiệt!"
+    }
+}
+
+//                             TUNG XÚC XẮC
+
+function tungXucXac() {
+    const soXucXac = document.querySelector('#soXucXac').value;
+    const ketQuaXucXac = document.querySelector('#ketQuaXucXac');
+    const hinhAnhXucXac = document.querySelector('#hinhAnhXucXac');
+    const giaTriMang = [];
+    const hinhAnh = [];
+
+    for (let i = 0; i < soXucXac; i++) {
+        const giaTri = Math.floor(Math.random() * 6) + 1;
+        giaTriMang.push(giaTri);
+        hinhAnh.push(`<img src="/diceImages/${giaTri}.png">`);
+    }
+
+    ketQuaXucXac.textContent = `Xúc xắc: ${giaTriMang.join(',')}`;
+    hinhAnhXucXac.innerHTML = hinhAnh.join(' | ');
+}
